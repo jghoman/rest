b617ed8363b99ac55e0ca927e3b186b2c3455611
Vlad Faust <mail@vladfaust.com>
2019-01-31 14:44:18 +0300

    wip (Rescuers::REST): print error on fallback

--- src/onyx-rest/rescuers/rest.cr
+++ src/onyx-rest/rescuers/rest.cr
@@ -4,7 +4,8 @@ module Onyx::REST
   module Rescuers
     class REST < HTTP::Rescuers::Silent(Onyx::REST::Error)
       def fallback(context, error)
-        raise error
+        context.response.status_code = error.code
+        context.response << error.code << " " << error.message
       end
     end
   end
